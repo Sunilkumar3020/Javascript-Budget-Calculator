409a93786f4c32b8475eb0f5aaed3562d928d278
Sunil kumar <sunilkumar3020@gmail.com>
2019-02-05 19:23:02 +0530

    Some files modified and new files added

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCss = require('gulp-clean-css'),
+    htmlMin = require('gulp-htmlmin'),
+    jsUglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+    imageminPngquant = require('imagemin-pngquant'),
+    watch = require('gulp-watch');
+    
+    
+
+
+
+
+gulp.task('html', function(){
+   return gulp.src('*.html')
+        .pipe(htmlMin())
+        .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('style', function(){
+   return gulp.src('css/*.css')
+        .pipe(autoprefixer())
+        .pipe(cleanCss())
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+
+gulp.task('js', function(){
+    return gulp.src('js/*.js')
+        .pipe(jsUglify())
+        .pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('image', function(){
+    return gulp.src('img/*.{png,jpeg,jpg,gif,svg}')
+        .pipe(imagemin(
+                [
+                    imagemin.gifsicle(),
+                    imagemin.jpegtran(),
+                    imagemin.optipng(),
+                    imagemin.svgo(),
+                    imageminPngquant(),
+                    imageminJpegRecompress()
+                ]
+        ))
+        .pipe(gulp.dest('dist/img'))
+});
+
+
+// WATCH TATSK
+
+gulp.task('watch', function(){
+    gulp.watch('*.html').gulp.series('html');
+    gulp.watch('css/*.css').gulp.series('style');
+    gulp.watch('js/*.js').gulp.series('js');
+    gulp.watch('img/*.{png,jpeg,jpg,gif,svg}').gulp.series('image');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
